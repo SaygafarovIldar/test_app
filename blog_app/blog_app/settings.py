61fb459643dd040fdd027278c5f9116b622a8308
Ildar <sayildar17@gmail.com>
2023-03-03 11:44:06 +0500

    add host

--- blog_app/blog_app/settings.py
+++ blog_app/blog_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!#$riyeem339l8^!nb80@c%_eks$lctk$+=4ipx9^mfe)$h!(t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sayildar17.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
