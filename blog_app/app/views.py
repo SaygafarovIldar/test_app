628a6e2a8cf36f139509cddd3e921c484b8b25ed
Ildar <sayildar17@gmail.com>
2023-03-03 13:41:00 +0500

    showed categories in template

--- blog_app/app/views.py
+++ blog_app/app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from app.models import Category
 
 # Create your views here.
 
 
 def home_view(request):
-    return render(request, "pages/index.html")
+    categories = Category.objects.all()
+    context = {
+        "categories": categories
+    }
+    return render(request, "pages/index.html", context)
