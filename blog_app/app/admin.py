05e9f03f4c8d3a5b83ab160a20dbda5f5cee2430
Ildar <sayildar17@gmail.com>
2023-03-03 13:33:52 +0500

    added model Category

--- blog_app/app/admin.py
+++ blog_app/app/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
+from .models import Category
 
 # Register your models here.
-###sdfsd
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "slug")
+    list_display_links = ("pk", "title")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Category, CategoryAdmin)
--- blog_app/app/models.py
+++ blog_app/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(verbose_name="Название", max_length=100, help_text="Напишите название для категории")
+    slug = models.SlugField(verbose_name="Слаг категории", max_length=100,
+                            help_text="Переводит название категории, на английские буквы для формирования ссылки")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
